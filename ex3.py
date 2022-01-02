5dac3cc6fa5387aa0666c65693036cd6bee5f7e9
rom <rom.sharon98@gmail.com>
2022-01-02 23:02:24 +0200

    model works but return shity results

--- ex3.py
+++ ex3.py
@@ -5,8 +5,9 @@ import pickle
 import numpy as np
 
 RARE_WORD = 3
-EPSILON = 0.000001
-LAMBDA = 2
+EPSILON = 0.1
+LAMBDA = 1
+K = 5
 CORPUS = "dataset/develop.txt"
 RESULTS = "results.pkl"
 CLUSTERS = "dataset/topics.txt"
@@ -26,7 +27,7 @@ class EM(object):
 
         for document_index, document in enumerate(self.__parse_corpus):
             for word in self.__parse_corpus[document][1]:
-                self.__n_t_k[document_index][self.__words[word]] += 1
+                self.__n_t_k[document_index][self.__words[word]] = self.__parse_corpus[document][1].count(word)
             self.__n_t[document_index] = len(self.__parse_corpus[document][1])
 
         self.__w_t_i = np.zeros((len(self.__parse_corpus), len(self.__clusters)))
@@ -38,57 +39,59 @@ class EM(object):
     def m_step(self):
         self.calculate_alpha()
         self.__p_i_k = (np.dot(self.__w_t_i.T, self.__n_t_k) + LAMBDA) / (
-                np.dot(self.__w_t_i.T, self.__n_t) + len(self.__parse_corpus) * LAMBDA)
+                np.dot(self.__w_t_i.T, self.__n_t) + len(self.__words) * LAMBDA)
 
     def calculate_alpha(self):
-        self.__alpha = np.sum(self.__w_t_i) / len(self.__parse_corpus)
+        self.__alpha = np.sum(self.__w_t_i, axis=0) / len(self.__parse_corpus)
         self.__alpha = np.maximum(self.__alpha, EPSILON)
         self.__alpha /= np.sum(self.__alpha)
 
-    def e_step(self):
-        z = self.z_matrix = self.calculate_z()
-        m = self.m_matrix = np.max(z, axis=1).reshape(len(self.__parse_corpus), 1)
-        for i in range(m.shape[0]):
-            for j in range(z.shape[1]):
-                z[i][j] -= m[i]
-        z = z / 4
-        e_mat = np.where(z < (-1) * 10, np.exp(z), np.exp(z))
+    def e_step(self, z, m):
+        e_mat = np.where(z - m < (-1) * K, 0, np.exp(z - m))
         e_sum = np.sum(e_mat, axis=1)
-        for i in range(e_mat.shape[0]):
-            e_mat[i] /= e_sum[i]
-        print(e_mat)
-        self.__w_t_i = e_mat
+        self.__w_t_i = e_mat / np.column_stack([e_sum for _ in range(e_mat.shape[1])])
+        return
 
     def calculate_z(self):
         first = np.dot(self.__n_t_k, np.log(self.__p_i_k.T))
         second = np.broadcast_to(np.log(self.__alpha), (len(self.__parse_corpus), len(self.__clusters)))
         return first + second
 
-    # def train(self):
-    #     prev_prep = float('inf')
-    #     cur_prep = float('inf')
-    #     while prev_prep - cur_prep > EPSILON or cur_prep == float('inf'):
-    #         self.e_step()
-    #         self.m_step()
-    #         # print("acc", self.accuracy())
-    #         print("likelihood", self.log_likelihood())
-    #         prev_prep = cur_prep
-    #         cur_prep = self.perplexity()
-    #         print("cur_prep", cur_prep)
-    #
-    # def log_likelihood(self):
-    #     boolean_table = self.z_matrix - self.m_matrix >= (-1) * 10
-    #     array = np.zeros((len(self.__parse_corpus), 1))
-    #     for t in range(len(self.__parse_corpus)):
-    #         for i in range(len(self.__clusters)):
-    #             if boolean_table[t][i]:
-    #                 array[t] += np.exp(self.z_matrix[t][i] - self.m_matrix[t])
-    #     return np.sum(np.log(array) + self.m_matrix)
-    #
-    # def perplexity(self):
-    #     log_likelihood = self.log_likelihood()
-    #     return np.power(2, -1 * log_likelihood / np.sum(self.nt))
-    #
+    def calculate_m(self, z):
+        return np.max(z, axis=1).reshape(len(self.__parse_corpus), 1)
+
+    def train(self):
+        current = np.PINF
+        prev = np.PINF
+        check_log_likelihood = []
+        check_underflow_e = []
+        i = 0
+        while prev - current > EPSILON or current == np.PINF:
+            print(i)
+            print(current)
+            i += 1
+            z = self.calculate_z()
+            m = self.calculate_m(z)
+            self.e_step(z, m)
+            self.m_step()
+            prev = current
+
+            # calculate log likelihood
+            underflow_e = self.underflow_e_step(z, m)
+            current = np.power(np.e, (-1 / np.sum(self.__n_t)) * underflow_e)
+
+            check_underflow_e.append(underflow_e)
+            check_log_likelihood.append(current)
+        print(check_log_likelihood)
+        print(check_underflow_e)
+
+    def underflow_e_step(self, z, m):
+        result = np.zeros((len(self.__parse_corpus), 1))
+        for t, _ in enumerate(self.__parse_corpus):
+            for i, _ in enumerate(self.__clusters):
+                if z[t][i] - m[t] >= K * -1:
+                    result[t] += np.exp(z[t][i] - m[t])
+        return np.sum(np.log(result) + m)
 
 
 def get_rare_words(content):
@@ -123,6 +126,7 @@ if __name__ == "__main__":
         loaded_dict = pickle.load(f)
         clusters = import_clusters()
         em = EM(loaded_dict, clusters)
+        em.train()
     # with open(CORPUS, 'r') as file:
     #     content = file.read()
     #     rare_words = get_rare_words(content)
